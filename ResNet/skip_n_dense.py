b569567e66332c23457c15883428be4b71f4f463
Tavernier Antoine DESKTOP <tavernier.antoine@hotmail.fr>
2019-12-26 20:12:25 +0100

    [Resnet] Skip nth dense

--- ResNet/skip_n_dense.py
+++ ResNet/skip_n_dense.py
@@ -11,12 +11,13 @@ import tensorflow.keras as keras
 import matplotlib.pyplot as plt
 from tensorflow_core.python.keras.regularizers import L1L2
 
-def shift_out(new_output, last_output, penultimate_output):
-    penultimate_output = last_output
-    last_output = new_output
+def shift_out(last_output, penultimate_output, i):
+    tmp_out = last_output
+    last_output = Add(name=f"Add_{i}")([last_output, penultimate_output])
+    penultimate_output = tmp_out
     return last_output, penultimate_output
 
-def create_model(depth: int = 16):
+def create_model(depth, jumps):
     input_layer = Input((28, 28))
     flatten_layer_output = Flatten(name="flatten")(input_layer)
 
@@ -26,24 +27,17 @@ def create_model(depth: int = 16):
     for i in range(depth):
         if penultimate_output is not None:
 
-            last_output, penultimate_output = shift_out(Add(name=f"Add_{i}")([last_output, penultimate_output]), last_output, penultimate_output)
-            
-            last_output = Dense(784, activation=linear, name=f"Dense_{i}", kernel_regularizer=L1L2(l2=0.001 / depth))(last_output)
-            last_output = BatchNormalization()(last_output)
-            last_output = Activation(activation=relu, name=f"Activation_{i}")(last_output)
-
-            last_output = Dense(784, activation=linear, name=f"Dense_{i}_prime", kernel_regularizer=L1L2(l2=0.001 / depth))(last_output)
-            last_output = BatchNormalization()(last_output)
-            last_output = Activation(activation=relu, name=f"Activation_{i}_prime")(last_output)
+            last_output, penultimate_output = shift_out(last_output, penultimate_output, i)
+            for j in range(jumps):
+                last_output = Dense(784, activation=linear, name=f"Dense_{i}_{j}", kernel_regularizer=L1L2(l2=0.001 / depth))(last_output)
+                # last_output = BatchNormalization(name=f"BatchNormalization_{i}_{j}")(last_output)
+                # last_output = Activation(activation=relu, name=f"Activation_{i}_{j}")(last_output)
         else:
             penultimate_output = last_output
-            last_output = Dense(784, activation=linear, name=f"Dense_{i}", kernel_regularizer=L1L2(l2=0.001 / depth))(last_output)
-            last_output = BatchNormalization()(last_output)
-            last_output = Activation(activation=relu, name=f"Activation_{i}")(last_output)
-
-            last_output = Dense(784, activation=linear, name=f"Dense_{i}_prime", kernel_regularizer=L1L2(l2=0.001 / depth))(last_output)
-            last_output = BatchNormalization()(last_output)
-            last_output = Activation(activation=relu, name=f"Activation_{i}_prime")(last_output)
+            for j in range(jumps):
+                last_output = Dense(784, activation=linear, name=f"Dense_{i}_{j}", kernel_regularizer=L1L2(l2=0.001 / depth))(last_output)
+                # last_output = BatchNormalization(name=f"BatchNormalization_{i}_{j}")(last_output)
+                # last_output = Activation(activation=relu, name=f"Activation_{i}_{j}")(last_output)
 
     
     last_output = Add(name=f"Add_output")([last_output, penultimate_output])
@@ -52,7 +46,7 @@ def create_model(depth: int = 16):
     model = Model(input_layer, output_tensor)
 
     model.compile(loss=sparse_categorical_crossentropy,
-                  optimizer=Adam(),
+                  optimizer=Adam(lr=0.001),
                   metrics=[sparse_categorical_accuracy])
     return model
 
@@ -61,7 +55,7 @@ if __name__ == "__main__":
     (x_train, y_train), (x_val, y_val) = fashion_mnist.load_data()
     x_train = x_train / 255.0
     x_val = x_val / 255.0
-    model = create_model()
+    model = create_model(3, 6)
     print(model.summary())
     plot_model(model, "residual_dense.png")
     model.fit(x_train, y_train, validation_data=(x_val, y_val),
