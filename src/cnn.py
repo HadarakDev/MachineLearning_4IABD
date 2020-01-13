7da10cc245b42a134e8b6d9cb12beef19106d62b
HadarakDev <nico_roche@hotmail.fr>
2020-01-13 18:26:52 +0100

    new sparse norm cnn models

--- src/cnn.py
+++ src/cnn.py
@@ -38,8 +38,9 @@ def cnn_model_fit(model, X_param, Y_param, batch_size_param, epochs_param, save_
 
     log_dir = "..\\models\\cnn_sparse\\" + save_path_info
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-    earlystop_callback = EarlyStopping(monitor='sparse_categorical_accuracy', min_delta=0.0001, patience=10)
-    model.fit(X_param, Y_param, batch_size=batch_size_param, verbose=1, epochs=epochs_param, callbacks=[tensorboard_callback, earlystop_callback], validation_split=0.2)
+    earlystop_callback = EarlyStopping(monitor='sparse_categorical_accuracy', min_delta=0.0001, patience=5)
+    earlystop_val_callback = EarlyStopping(monitor='val_sparse_categorical_accuracy', min_delta=0.0001, patience=5)
+    model.fit(X_param, Y_param, batch_size=batch_size_param, verbose=1, epochs=epochs_param, callbacks=[tensorboard_callback, earlystop_callback, earlystop_val_callback], validation_split=0.2)
     model.save(save_path)
     return model
 
