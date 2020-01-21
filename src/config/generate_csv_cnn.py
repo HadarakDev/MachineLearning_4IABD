06a1d0a4f9e7e49d5f70e272cd9b160d16c583ff
Uranium2 <tavernier.antoine@hotmail.fr>
2020-01-21 22:07:39 +0100

    generate config cnn

--- src/config/generate_csv_cnn.py
+++ src/config/generate_csv_cnn.py
@@ -17,7 +17,7 @@ bool_l=[False]
 l1l2 = [0]
 dropout = [0]
 kernel_shape = [2]
-nb_neurons = [86]
+nb_neurons = [24]
 nb_layers = [4]
 pooling = ["max_pool", "avg_pool"]
 
@@ -28,7 +28,6 @@ batch_size_param = []
 epochs_param = []
 lr = []
 isGray_param = []
-isSparse_param = []
 isNorm_param = []
 kernel_shape_param = []
 pooling_param = []
@@ -45,7 +44,6 @@ for i in range(10000):
     epochs_param.append(random.choice(epochs))
     lr.append(random.choice(lrs))
     isGray_param.append(random.choice(bool_l))
-    isSparse_param.append(random.choice(bool_l))
     isNorm_param.append(random.choice(bool_l))
 
     kernel_shape_param.append(random.choice(kernel_shape))
@@ -66,10 +64,10 @@ for i in range(10000):
 # Calling DataFrame constructor after zipping 
 # both lists, with columns specified 
 l = list(zip(activation_param, optimizer_param, loss_param, epochs_param, batch_size_param,
-                lr, layers_param, kernel_shape_param, pooling_param, isGray_param, isSparse_param,
+                lr, layers_param, kernel_shape_param, pooling_param, isGray_param,
                 isNorm_param, dropout_param, l1_param, l2_param))
 list_set = set(l) 
 unique_list = (list(list_set))
 df = pd.DataFrame(unique_list, 
-               columns =["activation", "optimizer", "loss", "epochs", "batch-size", "learning-rate", "layers", "kernel-shape", "pooling", "isGray", "isSparse", "isNorm", "Dropout", "L1", "L2"]) 
+               columns =["activation", "optimizer", "loss", "epochs", "batch-size", "learning-rate", "layers", "kernel-shape", "pooling", "isGray", "isNorm", "Dropout", "L1", "L2"]) 
 df.to_csv("../../config/cnn.csv", index=False)
